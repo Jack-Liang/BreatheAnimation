85b672f1f40ac725361e5b1f6547427c46e1a5cd
Jack-Liang <jack19940101@gmail.com>
2022-09-02 21:46:39 +0800

    Add files via upload

---
+++ BreatheAnimation/BreatheAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreatheAnimationApp.swift
+//  BreatheAnimation
+//
+//  Created by Liang Jack on 2022/8/31.
+//
+
+import SwiftUI
+
+@main
+struct BreatheAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
