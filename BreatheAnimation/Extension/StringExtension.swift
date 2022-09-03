16cff9a70c3bd7ecb2f2e94de1d12a97d86a0c0b
Jack-Liang <jack19940101@gmail.com>
2022-09-03 21:32:36 +0800

    Create StringExtension.swift

---
+++ BreatheAnimation/Extension/StringExtension.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  BreatheAnimation
+//
+//  Created by Liang Jack on 2022/9/3.
+//
+
+import Foundation
+extension String {
+    var localized: String {
+        NSLocalizedString(self, comment: "")
+    }
+}
